2f8a27475dca046e72480d66f0dde7d35eb42fc1
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2019-07-26 10:59:08 -0700

    partial Stretch Progress. added form field and query.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TabNav from './components/TabNav.js';
 import Header from './components/Header.js';
 import AppRouter from './components/AppRouter.js';
+import SearchForm from './components/SearchForm.js';
+import CharacterCard from './components/CharacterCard.js';
+import axios from 'axios';
 
 export default function App() {
+
+  const [searchQuery, setSearchQuery] = useState();
+  const [queryData, setQueryData] = useState();
+
+  const onSearch = (event, query) => {
+    event.preventDefault();
+    console.log(query);
+    setSearchQuery(query)
+  }
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://rickandmortyapi.com/api/${searchQuery.type}/?name=${searchQuery.name}`
+      )
+      .then(result => {
+        setQueryData(result.data);
+      })
+      .catch(error => {
+        console.log('error in app.js', error);
+      });
+  }, [searchQuery])
+
   return (
     <main>
       <Header />
       <TabNav />
       <AppRouter />
+      <section className='search-form'>
+        <SearchForm
+          onSearch={onSearch}
+        />
+      </section>
+      {/* <section className='character-list grid-view'>
+        {queryData.map((characterData, index) => (
+          <CharacterCard key={index} characterData={characterData} />
+        ))}
+      </section> */}
     </main>
     )
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,20 +2,27 @@ import React, { useState } from 'react';
 
 export default function SearchForm({ onSearch }) {
   const [query, setQuery] = useState({
-    name: ""
+    name: "",
+    type: ''
   })
   const handleInputChange = (event) => {
-    setQuery({ ...query, name: event.target.value })
+    setQuery({ ...query, [event.target.name]: event.target.value })
   }
 
   return (
     <section className="search-form">
-      <form onSubmit={() => onSearch(query)}>
+      <form onSubmit={(event) => onSearch(event, query)}>
         <input
           onChange={handleInputChange}
           placeholder="name"
           value={query.name}
           name="name"
+        />
+        <input
+          onChange={handleInputChange}
+          placeholder="type - character, location, episode"
+          value={query.type}
+          name="type"
         />
         <button type="submit">Search</button>
       </form>
