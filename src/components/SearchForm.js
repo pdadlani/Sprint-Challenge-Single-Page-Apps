f21d96723a7dac9503c32e0eebabc4dee7edb271
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2019-08-01 20:07:59 -0700

    changed search form to have dropdown. trial for bw.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,11 +3,16 @@ import React, { useState } from 'react';
 export default function SearchForm({ onSearch }) {
   const [query, setQuery] = useState({
     name: "",
-    type: ''
+    type: 'character'
   })
   const handleInputChange = (event) => {
+    console.log('event.target.name', event.target.name)
+    console.log('event.target.value', event.target.value)
+
+
     setQuery({ ...query, [event.target.name]: event.target.value })
   }
+
 
   return (
     <section className="search-form">
@@ -18,12 +23,24 @@ export default function SearchForm({ onSearch }) {
           value={query.name}
           name="name"
         />
-        <input
+
+        <div className='type'>
+          <label>
+            Type:
+              <select onChange={handleInputChange} id='type' name='type'>
+              <option value='character'>Character</option>
+              <option value='episode'>Episode</option>
+              <option value='location'>Location</option>
+            </select>
+          </label>
+        </div>
+
+        {/* <input
           onChange={handleInputChange}
           placeholder="type - character, location, episode"
           value={query.type}
           name="type"
-        />
+        /> */}
         <button type="submit">Search</button>
       </form>
     </section>
